49d64845640e533e8ebcaa3b4eea43780f142570
iqaenganer <daw.camalian@yandex.ru>
2021-07-19 11:26:11 +0300

    COm2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+         int balance = 2000;
+         int Adding = 80000;
+         int mel = 100;
+        int basicbalance;
+        int bbalance;
+         if (Adding > 1000)
+         {
+              bbalance = Adding / mel;
+              basicbalance = bbalance + Adding + balance;
+         }
+         else {
+              basicbalance = Adding + balance;
+         }
+
+        System.out.println(basicbalance);
+
+    }
 }
